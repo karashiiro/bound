8ca9a9b11a2623f610b6a5be7144f1e76813a604
karashiiro <49822414+karashiiro@users.noreply.github.com>
2026-03-23 01:18:02 +0800

    chore(discord): initialize @bound/discord package

---
+++ packages/discord/src/index.ts
@@ -0,0 +1,9 @@
+// Export bot
+export { DiscordBot } from "./bot";
+export { shouldActivate } from "./bot";
+
+// Export allowlist
+export { isAllowlisted } from "./allowlist";
+
+// Export thread mapping
+export { findOrCreateThread, mapDiscordUser } from "./thread-mapping";
